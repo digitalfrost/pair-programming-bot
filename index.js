2284d017b08b96ac5d5c7dca2e025684808f85ca
Gerald <gerald.hofmaier@gmail.com>
2015-09-19 00:21:30 +0200

    quotes at random interval

---
+++ index.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+var say = require('say');
+var _ = require('underscore');
+
+var quotes = ["How long since you checked something in?",
+              "You're going to refactor that, right?",
+              "What's the shortest line to the end of this task?",
+              "You ain't gonna need it",
+              "Have you got a test for that?"];
+
+
+
+function sayQuote() {say.speak('Leo', _.sample(quotes));}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+(function loop() {
+    var minute = 60000;
+    var every = getRandomInt(1,3) * minute
+    setTimeout(function() {
+            //alert('A');
+            sayQuote();
+            loop();
+    }, every);
+}());
